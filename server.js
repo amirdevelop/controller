feac6822dd29392c165bd416f8ab5396a6788753
amirdevelop <108542219+amirdevelop@users.noreply.github.com>
2022-07-01 21:34:23 +0430

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 const wss = new socket.Server({server});
 const chatId = '2126570077'
 const token = '5503805282:AAEi2yYiCiGg5vZIDH_CvqJJu-k1LTSio68'
-const serverAddr = ''
+const serverAddr = 'https://dashing-marble-owl.glitch.me/'
 const bot = new TelegramBot(token, {polling: true});
 
 // request -
